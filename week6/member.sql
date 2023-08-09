27a34db765d99981252fdb3fe06bfd0ee42879aa
luluching <109401006+chuchiang@users.noreply.github.com>
2023-08-09 23:48:37 +0800

    upload week6

---
+++ week6/member.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: SELECT*FROM member
+LIMIT 0, 1000
+
+-- Date: 2023-08-09 23:37
+*/
+INSERT INTO `` (`id`,`name`,`username`,`password`,`follower_count`,`time`) VALUES (1,'test2','test','test',10,'2023-08-03 23:48:16');
+INSERT INTO `` (`id`,`name`,`username`,`password`,`follower_count`,`time`) VALUES (2,'cindy','service1','service1',5,'2023-08-03 23:48:30');
+INSERT INTO `` (`id`,`name`,`username`,`password`,`follower_count`,`time`) VALUES (3,'betty','service2','service2',8,'2023-08-03 23:48:42');
+INSERT INTO `` (`id`,`name`,`username`,`password`,`follower_count`,`time`) VALUES (4,'tommy','service3','service3',2,'2023-08-03 23:48:51');
+INSERT INTO `` (`id`,`name`,`username`,`password`,`follower_count`,`time`) VALUES (5,'kevin','service4','service4',7,'2023-08-03 23:49:01');
+INSERT INTO `` (`id`,`name`,`username`,`password`,`follower_count`,`time`) VALUES (20,'123','123','123',0,'2023-08-09 08:53:22');
+INSERT INTO `` (`id`,`name`,`username`,`password`,`follower_count`,`time`) VALUES (21,'evy','service5','service5',0,'2023-08-09 19:14:02');
+INSERT INTO `` (`id`,`name`,`username`,`password`,`follower_count`,`time`) VALUES (22,'penny','service6','service6',0,'2023-08-09 23:16:30');
+INSERT INTO `` (`id`,`name`,`username`,`password`,`follower_count`,`time`) VALUES (23,'宇宙人','yahoo','yuotube',0,'2023-08-09 23:25:50');
---
+++ week6/message.sql
@@ -0,0 +1,19 @@
+/*
+-- Query: SELECT*FROM message
+LIMIT 0, 1000
+
+-- Date: 2023-08-09 23:36
+*/
+INSERT INTO `` (`id`,`member_id`,`content`,`like_count`,`time`) VALUES (1,1,'HELLO','10','2023-08-03 23:57:04');
+INSERT INTO `` (`id`,`member_id`,`content`,`like_count`,`time`) VALUES (2,1,'HI','2','2023-08-03 23:57:14');
+INSERT INTO `` (`id`,`member_id`,`content`,`like_count`,`time`) VALUES (3,2,'HELLO cindy','25','2023-08-03 23:57:23');
+INSERT INTO `` (`id`,`member_id`,`content`,`like_count`,`time`) VALUES (4,3,'HELLO betty','13','2023-08-03 23:57:33');
+INSERT INTO `` (`id`,`member_id`,`content`,`like_count`,`time`) VALUES (5,4,'HELLO tommy','15','2023-08-03 23:57:42');
+INSERT INTO `` (`id`,`member_id`,`content`,`like_count`,`time`) VALUES (6,5,'HELLO kevin','3','2023-08-03 23:57:50');
+INSERT INTO `` (`id`,`member_id`,`content`,`like_count`,`time`) VALUES (7,1,'test test','1','2023-08-07 09:55:44');
+INSERT INTO `` (`id`,`member_id`,`content`,`like_count`,`time`) VALUES (10,2,'你好','0','2023-08-09 17:50:41');
+INSERT INTO `` (`id`,`member_id`,`content`,`like_count`,`time`) VALUES (11,1,'123','0','2023-08-09 18:30:14');
+INSERT INTO `` (`id`,`member_id`,`content`,`like_count`,`time`) VALUES (12,1,'123','0','2023-08-09 18:31:31');
+INSERT INTO `` (`id`,`member_id`,`content`,`like_count`,`time`) VALUES (13,1,'123','0','2023-08-09 18:32:36');
+INSERT INTO `` (`id`,`member_id`,`content`,`like_count`,`time`) VALUES (14,1,'你好','0','2023-08-09 18:32:51');
+INSERT INTO `` (`id`,`member_id`,`content`,`like_count`,`time`) VALUES (15,1,'你好','0','2023-08-09 18:54:51');
